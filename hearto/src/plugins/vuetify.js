041a0cee8d7fedfe2e70276315fab564011fa1ea
ya95123 <ya95123@gmail.com>
2020-07-07 10:43:36 +0800

    adjust：font-family、color-settings

--- hearto/src/plugins/vuetify.js
+++ hearto/src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#5C4A72',
+        secondary: '#A8586D',
+        accent: '#FFFFFF',
+        error: '#F46A4E',
+        info: '#F4874B',
+        success: '#41B883',
+        warning: '#F4B15A'
+      }
+    }
+  }
 })
